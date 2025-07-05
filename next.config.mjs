f0db561c51aaf92beec79dcc463b930e54adaf6f
Ayon Sen <152070072+AkagamiShnaks@users.noreply.github.com>
2025-07-06 02:41:17 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,11 @@ const nextConfig = {
         hostname: "utfs.io",
         pathname: `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/*`,
       },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/f/*",
+      },
     ],
   },
   rewrites: () => {
